e3d5648278bf2062a8f3b2ab48133c0e91c5f8d4
junestag <sage.adams@gmail.com>
2016-08-04 01:33:59 -0700

    First website commit

---
+++ js/zaccordionUsage.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#slideshow").zAccordion({
+		width: 1200,
+		speed: 600,
+		slideClass: "slider",
+		slideWidth: 600,
+		height: 400,
+		trigger: "mouseover",
+		auto: false
+  });
+});
